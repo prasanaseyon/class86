12f15621a56d8943ac456c59dd87d23352c15be8
prasanaseyon <75750776+prasanaseyon@users.noreply.github.com>
2021-10-23 21:29:07 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCBsoFrNYZl_o0r9yBB-nKp8ck2rtZ-Oak",
+    authDomain: "story-c70af.firebaseapp.com",
+    projectId: "story-c70af",
+    storageBucket: "story-c70af.appspot.com",
+    messagingSenderId: "401437235001",
+    appId: "1:401437235001:web:267e16b850ce7911e9fae8"
+  };
